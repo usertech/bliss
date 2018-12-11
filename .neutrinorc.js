f52aeef292b5032bbe8463b141cf5cb36e4e206a
Jiri Brabec <brabeji@gmail.com>
2018-12-11 15:13:41 +0100

    Build for old browsers

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,10 +1,22 @@
 module.exports = {
-  use: [
-    [
-      '@neutrinojs/library',
-      {
-        name: '@usertech/bliss'
-      }
-    ]
-  ]
+	use: [
+		[
+			'@neutrinojs/library',
+			{
+				name: '@usertech/bliss',
+				babel: {
+					// Override options for babel-preset-env
+					presets: [
+						['babel-preset-env', {
+							// Passing in browser targets to babel-preset-env will replace them
+							// instead of merging them when using the 'web' target
+							targets: {
+								browsers: ["last 2 versions", "ie >= 7"],
+							}
+						}]
+					]
+				}
+			}
+		]
+	]
 };
